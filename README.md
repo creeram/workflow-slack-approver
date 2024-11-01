0fb3f234cab93abc970c6c1ad5b7630d8fd1016e
Shreeram Silwal <creeam.silwal@gmail.com>
2024-11-01 10:19:35 +0545

    Initial commit

---
+++ README.md
@@ -0,0 +1,51 @@
+# slack-approval
+
+custom action to send approval request to Slack
+
+
+
+- Post a message in Slack with a "Approve" and "Reject" buttons. 
+- Clicking on "Approve" will execute next steps.
+- Clicking on "Reject" will cause workflow to fail.
+
+# How To Use
+
+- First, create a Slack App and install in your workspace.
+- Second, add `chat:write` and `im:write` to OAuth Scope on OAuth & Permissions page.
+- Finally, **Enable Socket Mode**.
+
+```
+jobs:
+  approval:
+    runs-on: ubuntu-latest
+    steps:
+      - name: send approval
+        uses: creeram/slack-approver@v2.0.0
+        env:
+          SLACK_APP_TOKEN: ${{ secrets.SLACK_APP_TOKEN }}
+          SLACK_BOT_TOKEN: ${{ secrets.SLACK_BOT_TOKEN }}
+          SLACK_SIGNING_SECRET: ${{ secrets.SLACK_SIGNING_SECRET }}
+          SLACK_CHANNEL_ID: ${{ secrets.SLACK_CHANNEL_ID }}
+        timeout-minutes: 10
+```
+
+- Set environment variables
+
+  - `SLACK_APP_TOKEN`
+
+    - App-level tokens on `Basic Information page`. (starting with `xapp-` )
+
+  - `SLACK_BOT_TOKEN`
+
+    - Bot-level tokens on `OAuth & Permissions page`. (starting with `xoxb-` )
+
+  - `SLACK_SIGNING_SECRET`
+
+    - Signing Secret on `Basic Information page`.
+
+  - `SLACK_CHANNEL_ID`
+
+    - Channel ID for which you want to send approval.
+
+- Set `timeout-minutes`
+  - Set the time to wait for approval.
---
+++ src/index.ts
@@ -0,0 +1,188 @@
+import * as core from '@actions/core'
+import { App, BlockAction, LogLevel } from '@slack/bolt'
+import { WebClient } from '@slack/web-api'
+import { randomUUID } from 'crypto'
+
+const token = process.env.SLACK_BOT_TOKEN || ""
+const signingSecret =  process.env.SLACK_SIGNING_SECRET || ""
+const slackAppToken = process.env.SLACK_APP_TOKEN || ""
+const channel_id    = process.env.SLACK_CHANNEL_ID || ""
+const environment   = process.env.ENVIRONMENT || ""
+const url           = process.env.URL || ""
+const runport : any  = process.env.PORT || 3000
+const acceptValue : any = `${randomUUID()}-approve`;
+const rejectValue : any = `${randomUUID()}-reject`;
+
+const app = new App({
+  token: token,
+  signingSecret: signingSecret,
+  appToken: slackAppToken,
+  socketMode: true,
+  logLevel: LogLevel.DEBUG,
+});
+
+async function run(): Promise<void> {
+  try {
+    const web = new WebClient(token);
+
+    const github_server_url = process.env.GITHUB_SERVER_URL || "";
+    const github_repos = process.env.GITHUB_REPOSITORY || "";
+    const run_id = process.env.GITHUB_RUN_ID || "";
+    const actionsUrl = `${github_server_url}/${github_repos}/actions/runs/${run_id}`;
+    const workflow   = process.env.GITHUB_WORKFLOW || "";
+    const runnerOS   = process.env.RUNNER_OS || "";
+    const actor      = process.env.GITHUB_ACTOR || "";
+    const branch = process.env.GITHUB_REF || "";
+
+    (async () => {
+      await web.chat.postMessage({ 
+        channel: channel_id, 
+        text: "GitHub Actions Approval request",
+        blocks: [
+            {
+              "type": "section",
+              "text": {
+                  "type": "mrkdwn",
+                  "text": "GitHub Actions Approval Request",
+                }
+            },
+            {
+              "type": "section",
+              "fields": [
+                {
+                  "type": "mrkdwn",
+                  "text": `*GitHub Actor:* ${actor}`
+                },
+                {
+                  "type": "mrkdwn",
+                  "text": `*Repo:*\n${github_server_url}/${github_repos}`
+                },
+                { 
+                  "type": "mrkdwn",
+                  "text": `*Actions URL:*\n${actionsUrl}` 
+                },
+                { 
+                 "type": "mrkdwn",
+                 "text": `*Branch:* ${branch}`
+                },
+                {
+                  "type": "mrkdwn",
+                  "text": `*RunnerOS:* ${runnerOS}`
+                },
+                {
+                  "type": "mrkdwn",
+                  "text": `*GITHUB_RUNNER_ID:* ${run_id}`
+                },
+                
+                {
+                  "type": "mrkdwn",
+                  "text": `*ENV:* ${environment}`
+                },
+                {
+                  "type": "mrkdwn",
+                  "text": `*Workflow:* ${workflow}`
+                },
+                {
+                  "type": "mrkdwn",
+                  "text": `*URL: *${url}`
+                }
+              ]
+            },
+            {
+                "type": "actions",
+                "elements": [
+                    {
+                        "type": "button",
+                        "text": {
+                            "type": "plain_text",
+                            "emoji": true,
+                            "text": "Approve"
+                        },
+                        "style": "primary",
+                        "value": acceptValue,
+                        "action_id": "slack-approval-approve"
+                    },
+                    {
+                        "type": "button",
+                        "text": {
+                                "type": "plain_text",
+                                "emoji": true,
+                                "text": "Reject"
+                        },
+                        "style": "danger",
+                        "value": rejectValue,
+                        "action_id": "slack-approval-reject"
+                    }
+                ]
+            }
+        ]
+      });
+    })();
+
+    app.action('slack-approval-approve', async ({ack, client, body, logger, payload}) => {
+      try {
+        if((payload as any).value === acceptValue) {
+          await ack();
+          const response_blocks = (<BlockAction>body).message?.blocks
+          response_blocks.pop()
+          response_blocks.push({
+            'type': 'section',
+            'text': {
+              'type': 'mrkdwn',
+              'text': `Approved by <@${body.user.id}> `,
+            },
+          })
+  
+          await client.chat.update({
+            channel: body.channel?.id || "",
+            ts: (<BlockAction>body).message?.ts || "",
+            blocks: response_blocks
+          })
+          
+          process.exit(0);
+        }
+      } catch (error) {
+        logger.error(error)
+        process.exit(1)
+      }
+    });
+
+    app.action('slack-approval-reject', async ({ack, client, body, logger, payload}) => {
+      try {
+        if((payload as any).value === rejectValue) {
+          await ack();
+          const response_blocks = (<BlockAction>body).message?.blocks
+          response_blocks.pop()
+          response_blocks.push({
+            'type': 'section',
+            'text': {
+              'type': 'mrkdwn',
+              'text': `Rejected by <@${body.user.id}>`,
+            },
+          })
+
+          await client.chat.update({
+            channel: body.channel?.id || "",
+            ts: (<BlockAction>body).message?.ts || "",
+            blocks: response_blocks
+          });
+
+          process.exit(1);
+        }
+      } catch (error) {
+        logger.error(error)
+        process.exit(1)
+      }
+
+    });
+
+    (async () => {
+      const res = await app.start(runport);
+      console.log('Waiting Approval reaction.....');
+    })();
+  } catch (error) {
+    if (error instanceof Error) core.setFailed(error.message)
+  }
+}
+
+run()
